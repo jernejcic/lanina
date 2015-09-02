d61e1c1459e775fb00873155e4a3bdbc8a274920
Luke Jernejcic <ljernejcic@gmail.com>
2015-09-02 15:12:58 -0700

    Added iOS as a platform.

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.info({
+  id: 'com.jernejcic.lanina',
+  name: 'La Niña',
+  description: 'Weather app',
+  author: 'Luke Jernejcic',
+  email: 'support@jernejcic.com',
+  website: 'http://jernejcic.com'
+});
+
+// Set PhoneGap/Cordova preferences
+App.setPreference('BackgroundColor', '0xff0000ff');
+App.setPreference('HideKeyboardFormAccessoryBar', true);
+
